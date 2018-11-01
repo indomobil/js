bf30bbc1c29f19029ee1f711e0f333a50ac7468d
indomobil <44462825+indomobil@users.noreply.github.com>
2018-11-01 16:12:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,22 +23,3 @@ function openProduct (type) {
     $('#productMenu').fadeIn();
     $('#productMenu').load(base_url+'product/menu/'+type);
 }
-$(function(){
-    // init Isotope
-    var $grid = $('.grid').isotope({
-      // options
-    });
-    // filter items on button click
-    $('.product-filter').on( 'click', 'a', function() {
-      $('.product-filter a.active').removeClass('active');
-      $(this).addClass('active');
-
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({ filter: filterValue });
-    });
-
-    $('.product-listing-hider').on('click', 'a', function() {
-        $('.product-listing-container').css('height', 'auto');
-        $('.product-listing-hider').hide();
-    });
-});
